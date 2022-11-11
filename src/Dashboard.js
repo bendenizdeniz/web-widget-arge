3a582ddeaa2b1665e050c558f362ff2919a7bd4d
Oğuzhan Kumsal <oguzhan.kumsal@stoneity.com>
2022-11-11 14:26:47 +0300

    little changes :)

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,45 +1,11 @@
 import React, { useState } from 'react'
-import ReactDOMServer from 'react-dom/server';
-
-import PROCESS_TYPES from './PROCESS_TYPES.json';
 import './Dashboard.css';
-import { WidgetFlow } from './WidgetFlow';
 
 export const Dashboard = () => {
 
-    const [operationType, setOperationType] = useState();
-
-    const htmlContent = () => {
-        var operations = "<h1>Servicestone Web Widget</h1></br>";
-
-        for (const iterator of Object.keys(PROCESS_TYPES.processTypes)) {
-            operations +=
-                `
-                <input type="radio" id="example" /> 
-                <label for="example">${iterator}</label>
-                </br></br>
-                `
-                ;
-        }
-
-        operations += `
-            <button className="submitOperationType value="operationType" onClick={()=> {setOperationType()}}>Submit</button>
-        `
-        return operations;
-    }
-
-    const widgetFlow = () => {
-        return (
-            <WidgetFlow />
-        );
-    }
-
     return (
         <>
-            <div className="iframe-wrapper">
-                <iframe srcDoc={operationType ? htmlContent() : ReactDOMServer.renderToString(widgetFlow())} width="350" height="500"></iframe>
-                {/* <iframe src='https://base.stoneity.com/servicestone/' width="350" height="500"></iframe> */}
-            </div>
+
         </>
     )
 }
---
+++ src/Widget.jsx
@@ -0,0 +1,44 @@
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+    MainContainer, ChatContainer, ConversationHeader, MessageList, Message,
+    MessageInput, Button
+} from "@chatscope/chat-ui-kit-react";
+import { useChat } from "./ChatProvider";
+import "./Widget.less"
+
+export const Widget = ({ remoteName = "", messages = [], onSend }) => {
+
+    const { hide } = useChat();
+
+    return (<MainContainer>
+        <ChatContainer>
+            <ConversationHeader>
+                <ConversationHeader.Content userName="sadas">
+                    <span style={{
+                        color: "darkslategrey",
+                        alignSelf: "flex-center",
+                        fontWeight: "bold"
+                    }}>{remoteName}</span>
+                </ConversationHeader.Content>
+                <ConversationHeader.Actions>
+                    <Button onClick={hide} style={{ color: "darkslategrey", fontWeight: "600" }}>Hide</Button>
+                </ConversationHeader.Actions>
+            </ConversationHeader>
+
+            <MessageList>
+                {messages.map(message =>
+                    <Message key={message._id} model={message} />
+                )}
+
+            </MessageList>
+
+             <MessageInput placeholder="Type message here"
+                attachButton={false}
+                onSend={onSend}
+            /> 
+        </ChatContainer>
+    </MainContainer>);
+
+};
+
+
